014308eb31bed8e034326d1d97f3b3fe066e937c
Mauricio Soriano <mauricio2769@gmail.com>
2025-11-07 16:51:47 -0600

    reservar memoria para generar pdf

--- app/Http/Controllers/FiniquitoController.php
+++ app/Http/Controllers/FiniquitoController.php
@@ -327,6 +327,7 @@ class FiniquitoController extends Controller
 
     public function pdfCalculoFiniquito(Request $request, $finiquito_id = null)
     {
+        ini_set('memory_limit', '512M');
         // Si finiquito_id no viene en la URL, busca en el request body/query
         $idToUse = $finiquito_id ?? $request->input('finiquito_id');
 
